97130beb3dd72de67e4c2bb441e636ecfb07adc3
activateslikestacos <thetaco.dyndns@gmail.com>
2018-06-02 17:27:28 -0700

    Fixed config.php message

--- config.php
+++ config.php
@@ -5,13 +5,6 @@
  * Email: chris15588@gmail.com
  * File: config.php
  * Date: 6/1/2018
- * Description: My 'super-secret' config file containing all
- *   of my database information. I would prefer if you keep
- *   it secret, but even if you don't, the account used here
- *   will only work with the database assigned to it.
- *     You can browse my database using this url here, and the
- *     login information provided below, so that way you don't
- *     have to set it up!
  */
 
 /* !-- Global Config Options --! */
